a64e951612e5c4fe1fc9b18e0ddcc6c15acbb925
Zsanett Buko <zsanika@Zsanett-Air.hsd1.ut.comcast.net>
2022-03-27 15:27:14 -0600

    fixed_by_zsanettbuko

--- percentage.py
+++ percentage.py
@@ -1,7 +1,17 @@
 s = input()
 t = s.upper()
 
-for i in range(len(s)):
-    b = t.count(t[i])
-    print(s[i],b/len(s)*100)
+print(s.count("C"))
+print(s.count("A"))
+print(s.count("G"))
+print(s.count("T"))
 
+
+print("The percentage of C is")
+print(100*t.count("C")/len(t))
+print("The percentage of A is")
+print(100*t.count("A")/len(t))
+print("The percentage of G is")
+print(100*t.count("G")/len(t))
+print("The percentage of T is")
+print(100*t.count("T")/len(t))
